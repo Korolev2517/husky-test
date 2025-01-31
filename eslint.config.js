321eb151214839e2ff80cf900da27108f31296fa
kkorolev <kkorolev@ntr.ai>
2025-01-31 17:19:51 +0700

    feat-prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,11 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
+  { extends: ['plugin:prettier/recommended'] },
   { ignores: ['dist'] },
   {
     files: ['**/*.{js,jsx}'],
@@ -34,5 +35,6 @@ export default [
         { allowConstantExport: true },
       ],
     },
+    'prettier/prettier': 'error',
   },
-]
+];
